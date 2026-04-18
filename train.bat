09bd3081d928764438ad036a2800c05184b037f2
RichardsWorld <richardmovie2022@gmail.com>
2026-04-18 08:26:13 -0400

    Create train.bat for environment setup and training
    This batch file sets up the environment, configures the build, and runs the GigaLearnBot in training mode.

---
+++ train.bat
@@ -0,0 +1,21 @@
+@echo off
+REM Activate enviroment
+echo Activating enviroment
+call "C:\Users\YourUsername\anaconda3\Scripts\activate.bat" "C:\Users\YourUsername\anaconda3"
+call conda activate gigalearn_env
+
+REM Change to the build directory
+cd /d C:\Users\YourUsername\OneDrive\Documents\GigaLearnCPP-Leak\build
+
+REM echo Configuring...
+cmake .. -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DTorch_DIR="C:/Users/yiric/OneDrive/Documents/GigaLearnCPP-Leak/GigaLearnCPP/libtorch/share/cmake/Torch"
+echo Building...
+cmake --build . --config RelWithDebInfo --target GigaLearnBot
+
+REM Move to the RelWithDebInfo folder and run the bot (training mode)
+cd RelWithDebInfo
+echo Training...
+GigaLearnBot.exe
+pause
+
+REM Put this into your /build folder by the way, you can make a windows shortcut and drag it to your desktop folder for easy access.
