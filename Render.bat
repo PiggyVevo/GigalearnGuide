066c9f05151071deceea87de260aae5884f87023
RichardsWorld <richardmovie2022@gmail.com>
2026-04-18 08:32:16 -0400

    Refactor Render.bat for rendering and user instructions
    Updated the Render.bat script to streamline the rendering process and added important notes for users.

---
+++ Render.bat
@@ -0,0 +1,15 @@
+@echo off
+REM Activate enviroment
+echo Activating enviroment
+call "C:\Users\YourUsername\anaconda3\Scripts\activate.bat" "C:\Users\YourUsername\anaconda3"
+call conda activate gigalearn_env
+
+REM Change to the build directory
+cd /d C:\Users\YourUsername\OneDrive\Documents\GigaLearnCPP-Leak\build\RelWithDebInfo
+echo Rendering...
+GigaLearnBot.exe --render
+pause
+
+REM Put this into your /build folder by the way, you can make a windows shortcut and drag it to your desktop folder for easy access.
+REM ️‼️By the way, as you can see, I didn't put a configuring/build line into this file because I am assuming you already finished building while training.
+R️EM ‼️👀IMPORTANT: You need to start up the Visualizer.bat file too, as that renders it, this file just starts up a game to visualize, but doesn't render it.
