12bc5d7c120e9b8b1e4c6a5fe5d4698d70c19608
RichardsWorld <richardmovie2022@gmail.com>
2026-04-18 08:35:25 -0400

    Visualizer.bat up and running
    Updated instructions for running the visualizer and rendering.

--- Visualizer.bat
+++ Visualizer.bat
@@ -1,1 +1,6 @@
-R️EM IMPORTANT: You need to start up the Visualizer.bat file too, as that renders it, this file just starts up a game to visualize, but doesn't render it.
+echo Starting up the visualizer...
+python %0\..\RocketSimVis\src\main.py
+pause
+
+REM Put this into any folder, just make sure the RocketSimVis folder is in the folder this file is in, you can make a windows shortcut and drag it to your desktop folder for easy access.
+R️EM ‼️👀IMPORTANT: You need to start up the Render.bat file too. This script just renders whatever Render.bat sends it, and if Render.bat is not open, then it wouldn't render anything.
